f7922acb174ee86b03a368e1735d6f3d1a35ca0a
Agustin Vignoli <aguwebb@gmail.com>
2017-10-15 12:26:19 -0300

    initial commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+const configureStore = (initialState) => createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(reduxImmutableStateInvariant())
+);
+
+export default configureStore;
